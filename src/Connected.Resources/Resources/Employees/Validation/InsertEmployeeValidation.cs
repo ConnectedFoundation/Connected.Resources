2f34d9c6e2466cf8f3d9aeef22820e3bd9a7d819
mihaelocvirk <ocvirkmihael@gmail.com>
2026-04-03 12:47:21 +0200

    fix: employee validation handling incorrect ids

--- src/Connected.Resources/Resources/Employees/Validation/InsertEmployeeValidation.cs
+++ src/Connected.Resources/Resources/Employees/Validation/InsertEmployeeValidation.cs
@@ -4,6 +4,7 @@ using Connected.Resources.Resources.Employees.Dtos;
 using Connected.Resources.Resources.Persons;
 using Connected.Resources.Types.EmploymentTypes;
 using Connected.Resources.Types.JobPositions;
+using Connected.Services;
 using Connected.Services.Validation;
 
 namespace Connected.Resources.Resources.Employees.Validation;
@@ -18,31 +19,31 @@ internal sealed class InsertEmployeeValidation(IPersonService persons, IOrganiza
 
 		if (Dto.OrganizationUnit is not null)
 		{
-			if (await organizationUnits.Select(Dto) is null)
+			if (await organizationUnits.Select(Dto.CreatePrimaryKey(Dto.OrganizationUnit.Value)) is null)
 				throw ValidationExceptions.NotFound(nameof(IOrganizationUnit), Dto.OrganizationUnit);
 		}
 
 		if (Dto.JobPosition is not null)
 		{
-			if (await jobPositions.Select(Dto) is null)
+			if (await jobPositions.Select(Dto.CreatePrimaryKey(Dto.JobPosition.Value)) is null)
 				throw ValidationExceptions.NotFound(nameof(IJobPosition), Dto.JobPosition);
 		}
 
 		if (Dto.CostCenter is not null)
 		{
-			if (await costCenters.Select(Dto) is null)
+			if (await costCenters.Select(Dto.CreatePrimaryKey(Dto.CostCenter.Value)) is null)
 				throw ValidationExceptions.NotFound(nameof(ICostCenter), Dto.CostCenter);
 		}
 
 		if (Dto.Parent is not null)
 		{
-			if (await employees.Select(Dto) is null)
+			if (await employees.Select(Dto.CreatePrimaryKey(Dto.Parent.Value)) is null)
 				throw ValidationExceptions.NotFound(nameof(IEmployee), Dto.Parent);
 		}
 
 		if (Dto.EmploymentType is not null)
 		{
-			if (await employmentTypes.Select(Dto) is null)
+			if (await employmentTypes.Select(Dto.CreatePrimaryKey(Dto.EmploymentType.Value)) is null)
 				throw ValidationExceptions.NotFound(nameof(IEmploymentType), Dto.EmploymentType);
 		}
 	}
