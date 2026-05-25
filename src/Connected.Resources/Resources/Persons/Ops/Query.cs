6d1fc65793662758437069e3e0b087dc456de6ec
mihaelocvirk <ocvirkmihael@gmail.com>
2026-05-25 11:59:13 +0200

    fix: nullable persons query

--- src/Connected.Resources/Resources/Persons/Ops/Query.cs
+++ src/Connected.Resources/Resources/Persons/Ops/Query.cs
@@ -12,7 +12,7 @@ internal sealed class Query(IPersonCache cache)
 		var query = cache.AsQueryable();
 
 		if (Dto.Users is { Count: > 0 })
-			query = query.Where(x => Dto.Users.Contains(x.Id));
+			query = query.Where(x => x.User.HasValue && Dto.Users.Contains(x.User.Value));
 
 		return await cache.WithDto(Dto).AsEntities();
 	}
